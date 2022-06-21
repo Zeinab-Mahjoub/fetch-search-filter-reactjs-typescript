75efae7ee1bc26da8f7a91ec3600618c243ea1e1
Zeinab-Mahjoub <zmahjoub99@gmail.com>
2022-06-21 16:46:16 +0430

    feat(merge): merged Hamid's feature branch into the main branch which improved type speed and shortened the logic of search and filter

--- src/types/user.tsx
+++ src/types/user.tsx
@@ -1,9 +1,9 @@
 export type User = {
     "id": 497,
-            first_name: string,
-            last_name: string,
-            email: string,
-            gender: string,
-            ip_address: string,
-            avatar: string,
+    first_name: string,
+    last_name: string,
+    email: string,
+    gender: string,
+    ip_address: string,
+    avatar: string,
 }
